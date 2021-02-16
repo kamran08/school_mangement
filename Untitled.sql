ea7e3ce8ee22528d55dd3f89d371d313849c869a
itskamranweb <ahmedkamran265@gmail.com>
2021-02-16 21:51:44 +0600

    update

--- Untitled.sql
+++ Untitled.sql
@@ -7,39 +7,39 @@ CREATE TABLE `users` (
   `section_id` int(11),
   `createdBy` int(11) NOT NULL,
   `updatedBy` int(11) NOT NULL,
-  `created_at` timestamp DEFAULT NULL,
-  `updated_at` timestamp DEFAULT NULL
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
 CREATE TABLE `class` (
   `id` int(20) PRIMARY KEY NOT NULL,
   `name` varchar(191),
-  `created_at` timestamp DEFAULT NULL,
-  `updated_at` timestamp DEFAULT NULL
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
 CREATE TABLE `groups` (
   `id` int(20) PRIMARY KEY NOT NULL,
   `name` varchar(191) NOT NULL,
   `class_id` int(11) NOT NULL,
-  `created_at` timestamp DEFAULT NULL,
-  `updated_at` timestamp DEFAULT NULL
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
 CREATE TABLE `sections` (
   `id` int(20) PRIMARY KEY NOT NULL,
   `name` varchar(191) NOT NULL,
   `group_id` int(11) NOT NULL,
-  `created_at` timestamp DEFAULT NULL,
-  `updated_at` timestamp DEFAULT NULL
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
 CREATE TABLE `subjects` (
   `id` int(20) PRIMARY KEY NOT NULL,
   `name` varchar(191) NOT NULL,
   `group_id` int(11) NOT NULL,
-  `created_at` timestamp DEFAULT NULL,
-  `updated_at` timestamp DEFAULT NULL
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
 CREATE TABLE `exams` (
@@ -47,8 +47,8 @@ CREATE TABLE `exams` (
   `name` varchar(191),
   `year` year,
   `class_id` int(11),
-  `created_at` timestamp DEFAULT NULL,
-  `updated_at` timestamp DEFAULT NULL
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
 CREATE TABLE `results` (
@@ -58,24 +58,8 @@ CREATE TABLE `results` (
   `marks` double,
   `results` double,
   `grade` varchar(12),
-  `created_at` timestamp DEFAULT NULL,
-  `updated_at` timestamp DEFAULT NULL
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
-ALTER TABLE `users` ADD FOREIGN KEY (`class_id`) REFERENCES `class` (`id`);
 
-ALTER TABLE `groups` ADD FOREIGN KEY (`id`) REFERENCES `users` (`group_id`);
-
-ALTER TABLE `groups` ADD FOREIGN KEY (`class_id`) REFERENCES `class` (`id`);
-
-ALTER TABLE `sections` ADD FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
-
-ALTER TABLE `sections` ADD FOREIGN KEY (`id`) REFERENCES `users` (`section_id`);
-
-ALTER TABLE `results` ADD FOREIGN KEY (`student_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `groups` ADD FOREIGN KEY (`id`) REFERENCES `subjects` (`group_id`);
-
-ALTER TABLE `class` ADD FOREIGN KEY (`id`) REFERENCES `exams` (`class_id`);
-
-ALTER TABLE `subjects` ADD FOREIGN KEY (`id`) REFERENCES `results` (`subject_id`);
